ce7b45354addcf31ee67fca85e4f52a381e556e2
Ross <rosshamey@gmail.com>
2022-12-21 22:47:58 -0500

    s

---
+++ HelloWorld/dataTypes.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world!\n";                 // std::cout lives in the iostream library
+	std::cout << "It is very nice to meet you!\n"; // this is much easier to read
+	std::cout << "Yeah!\n";                        // don't you think so?
+	return 0;
+}
