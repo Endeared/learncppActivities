709c56e78c1a7ffcf14b36e186c96b801a248e1c
Ross <rosshamey@gmail.com>
2022-12-21 22:46:08 -0500

    Update HelloWorld.cpp

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
-	std::cout << "Hello world!";
+	std::cout << "Hello world!\n";                 // std::cout lives in the iostream library
+	std::cout << "It is very nice to meet you!\n"; // this is much easier to read
+	std::cout << "Yeah!\n";                        // don't you think so?
 	return 0;
 }
