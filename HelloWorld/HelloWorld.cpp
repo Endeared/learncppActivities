06f2a44178fa5c35284bb8c2e7342f872ec81890
Ross <rosshamey@gmail.com>
2022-12-21 23:16:46 -0500

    Update HelloWorld.cpp

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -8,15 +8,25 @@ int test()
 	int a = 1;
 	int b = 2;
 	std::cout << a << std::endl;
-	std::cout << "x is equal to: " << x;
+	std::cout << "x is equal to: " << x << '\n';
+	return 0;
+}
+
+int getInput()
+{
+	int x{};
+	std::cout << "Enter a number: ";
+	std::cin >> x;
+	std::cout << "You entered " << x << '\n';
 	return 0;
 }
 
 int main()
 {
-	std::cout << "Hello world!\n";                 // std::cout lives in the iostream library
-	std::cout << "It is very nice to meet you!\n"; // this is much easier to read
-	std::cout << "Yeah!\n";                        // don't you think so?
+	//std::cout << "Hello world!\n";                 // std::cout lives in the iostream library
+	//std::cout << "It is very nice to meet you!\n"; // this is much easier to read
+	//std::cout << "Yeah!\n";                        // don't you think so?
 	test();
+	getInput();
 	return 0;
 }
